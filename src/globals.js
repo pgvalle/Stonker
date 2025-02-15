84a16b0a235cf1d9b8f66eb3bfd40913540973d6
pgvalle <pgvalle.nogueira@gmail.com>
2025-02-15 17:03:14 -0300

    changed messages

--- src/globals.js
+++ src/globals.js
@@ -8,10 +8,10 @@ const db = new Database('./chats.db', OPEN_READWRITE | OPEN_CREATE);
 
 const msgs = {
   // info
-  DATA_SAVING_NOTICE: 'Our chat have an Id, and I saved it so that I can talk to you later',
-  HELP: 'This should be the help text.',
-  BYE_BYE: 'You deleted your id and settings. We hope to see you again... Bye...',
-  FORGETFUL: 'You are not registered. You will not receive any notifications.',
+  DATA_SAVING_NOTICE: `Our chat have an Id, and I saved it so that I can talk to you later`,
+  HELP: `This should be the help text.`,
+  BYE_BYE: `You deleted your id and settings. We hope to see you again... Bye...`,
+  FORGETFUL: `I do not know you. Do not give me orders. Send me a plain message first.`,
 
   // errors
   INVALID_COMMAND: 'This command is invalid',
