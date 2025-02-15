0f5d8d2a26fad4cd8e560c62931d6f5f67568cfb
pgvalle <pgvalle.nogueira@gmail.com>
2025-02-15 16:17:30 -0300

    refactoring and added message for non saved users trying to unsave themselves

--- src/globals.js
+++ src/globals.js
@@ -1,4 +1,4 @@
-const TelegramBot = require('node-telegram-bot-api'); // https://github.com/yagop/node-telegram-bot-api
+const TelegramBot = require('node-telegram-bot-api') // https://github.com/yagop/node-telegram-bot-api
 const { Database, OPEN_READWRITE, OPEN_CREATE } = require('sqlite3')
 
 const TELEGRAM_TK = '8075711316:AAGaVXIGrthKWKsmnbtEAa4ocdLnw-qYLRY'
@@ -9,8 +9,9 @@ const db = new Database('./chats.db', OPEN_READWRITE | OPEN_CREATE);
 const msgs = {
   // info
   DATA_SAVING_NOTICE: 'Our chat have an Id, and I saved it so that I can talk to you later',
-  HELP: 'This should be the help text',
+  HELP: 'This should be the help text.',
   BYE_BYE: 'You deleted your id and settings. We hope to see you again... Bye...',
+  FORGETFUL: 'You are not registered. You will not receive any notifications.',
 
   // errors
   INVALID_COMMAND: 'This command is invalid',
