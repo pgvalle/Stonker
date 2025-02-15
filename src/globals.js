22921fd69555256f87982f375d5b56c132ec4ebb
pgvalle <pgvalle.nogueira@gmail.com>
2025-02-15 14:23:31 -0300

    refactoring and added new messages

--- src/globals.js
+++ src/globals.js
@@ -1,15 +1,20 @@
 const TelegramBot = require('node-telegram-bot-api'); // https://github.com/yagop/node-telegram-bot-api
-const sql = require('sqlite3')
+const { Database, OPEN_READWRITE, OPEN_CREATE } = require('sqlite3')
 
 const TELEGRAM_TK = '8075711316:AAGaVXIGrthKWKsmnbtEAa4ocdLnw-qYLRY'
 
 const bot = new TelegramBot(TELEGRAM_TK, { polling: true });
-const db = new sql.Database('./chats.db', sql.OPEN_READWRITE | sql.OPEN_CREATE);
+const db = new Database('./chats.db', OPEN_READWRITE | OPEN_CREATE);
 
 const msgs = {
+  // info
   DATA_SAVING_NOTICE: 'Our chat have an Id, and I saved it so that I can talk to you later',
   HELP: 'This should be the help text',
-  INVALID_COMMAND: 'This command is invalid'
+  BYE_BYE: 'You deleted your id and settings. We hope to see you again... Bye...',
+
+  // errors
+  INVALID_COMMAND: 'This command is invalid',
+  WRONG_SYNTAX: 'Wrong command syntax'
 }
 
 module.exports = {
