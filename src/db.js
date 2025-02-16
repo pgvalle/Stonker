5cc0417efbf3d0e8470d417ace9b483de948056c
pgvalle <pgvalle.nogueira@gmail.com>
2025-02-16 18:51:03 -0300

    changed tables

--- src/db.js
+++ src/db.js
@@ -1,27 +1,25 @@
 const { Database, OPEN_READWRITE, OPEN_CREATE } = require('sqlite3')
-const db = new Database('./chats.db', OPEN_READWRITE | OPEN_CREATE);
+const db = new Database('./database.db', OPEN_READWRITE | OPEN_CREATE);
 
 // creating tables
 
 db.exec(`
-  CREATE TABLE IF NOT EXISTS user (
-    chat_id INTEGER NOT NULL PRIMARY KEY
-  )`
+    CREATE TABLE IF NOT EXISTS user (
+        chat INTEGER NOT NULL PRIMARY KEY
+    )`
 )
 
 db.exec(`
-  CREATE TABLE IF NOT EXISTS watcher (
-    ticker     VARCHAR(8) NOT NULL,
-    chat_id    INTEGER    NOT NULL REFERENCES chat(id) ON DELETE CASCADE
-    ref_price  REAL       NOT NULL,
-    change     REAL       NOT NULL,
-    ref_change REAL       NOT NULL,
-    PRIMARY KEY (ticker, chat_id)
-  )`
+    CREATE TABLE IF NOT EXISTS watcher (
+        ticker     VARCHAR(8) NOT NULL,
+        chat       INTEGER    NOT NULL REFERENCES user(chat) ON DELETE CASCADE,
+        ref_price  REAL       NOT NULL,
+        change     REAL       NOT NULL,
+        ref_change REAL       NOT NULL,
+        PRIMARY KEY (ticker, chat)
+    )`
 )
 
-// all queries located here to facilitate refactoring
-
 module.exports = {
-  db
+    db
 }
