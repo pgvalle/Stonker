f62f4625317b390672c07a396e9b570fa99ef5c5
pgvalle <pgvalle.nogueira@gmail.com>
2025-02-22 14:19:49 -0300

    better help texts for commands

--- src/cmds.js
+++ src/cmds.js
@@ -1,17 +1,21 @@
 const { db, sendMessage, addStockListener, fmtInvestment } = require('./core')
 
-const INVEST = `
-/invest STOCK VALUE DIFF
+const helps = {}
 
+helps.invest = `
+/invest STOCK VALUE DIFF
+\`\`\`
 Simulate stock investment.
 VALUE must be >= 1.00.
 DIFF must be > 0.00.
 DIFF is the gain/loss that triggers a notification.
-Only 2 decimals are used, so e.g 0.001 = $0.00.
-
+Only 2 decimals are used (e.g. 0.001 = $0.00).
+\`\`\`
 Examples:
+\`\`\`
   /invest AMD 1 1 # notify at $2.00 or $0.00
-  /invest NVDA 3.00 0.01 # notify at $3.01 or $2.99`
+  /invest NVDA 3.00 0.01 # notify at $3.01 or $2.99
+\`\`\``
 
 async function invest(user, args) {
     if (args.length !== 3) {
@@ -19,7 +23,7 @@ async function invest(user, args) {
         return
     }
 
-    await sendMessage(user, '*NOTE:* Only two decimal places are valid. E.g. 0.001 -> $0.00.')
+    await sendMessage(user, '*NOTE:* Only two decimal are used (e.g. 0.001 = $0.00).')
 
     const stockMIC = args[0].toUpperCase()
     const value = Number(args[1])
@@ -28,8 +32,7 @@ async function invest(user, args) {
     if (isNaN(value) || value < 1 || isNaN(diff) ||
         diff <= 0 || Number(diff.toFixed(2)) == 0)
     {
-        const reply = 'The first number must be >= $1.00 and the second one > $0.00.'
-        await sendMessage(user, reply)
+        await sendMessage(user, 'The first value must be >= $1.00 and the second one > $0.00.')
         return
     }
     
@@ -49,43 +52,19 @@ async function invest(user, args) {
     })
 }
 
-const DINVESTS = `
-/dinvests [STOCK ...]
-
-Delete your investments on specified stocks.
-Zero arguments deletes all investments.
-
-Examples:
-  /dinvest
-  /dinvest AMD TSLA NVDA`
-
-async function dinvests(user, args) {
-    var action = `DELETE FROM investment WHERE user = ${user}`
-    var reply = 'Now all your investments are gone.'
-
-    // delete specified investments
-    if (args.length > 0) {
-        const investments = `('` + args.join(`', '`).toUpperCase() + `')`
-        action = `DELETE FROM investment WHERE stockMIC IN ${investments} AND user = ${user}`
-        reply = 'Now those investments are gone.'
-    }
-
-    db.exec(action, async (_) => {
-        await sendMessage(user, reply)
-    })
-}
-
-const LINVESTS = `
-/linvests [STOCK ...]
-
+helps.linvest = `
+/linvest \\[STOCK ...]
+\`\`\`
 List your investments on specified stocks.
 Zero arguments lists all investments.
-
+\`\`\`
 Examples:
+\`\`\`
   /linvest
-  /linvest AMD TSLA NVDA`
+  /linvest AMD TSLA NVDA
+\`\`\``
 
-async function linvests(user, args) {
+async function linvest(user, args) {
     var action = `SELECT investment.*, stock.price FROM investment INNER JOIN stock
                   ON investment.stockMIC = stock.MIC WHERE investment.user = ${user}`
     var reply = 'Here are all your investments\n'
@@ -108,18 +87,48 @@ async function linvests(user, args) {
     })
 }
 
-const STOCKS = `
-/stocks [STOCK ...]
+helps.dinvest = `
+/dinvest \\[STOCK ...]
+\`\`\`
+Delete your investments on specified stocks.
+Zero arguments deletes all investments.
+\`\`\`
+Examples:
+\`\`\`
+  /dinvest
+  /dinvest AMD TSLA NVDA
+\`\`\``
 
+async function dinvest(user, args) {
+    var action = `DELETE FROM investment WHERE user = ${user}`
+    var reply = 'Now all your investments are gone.'
+
+    // delete specified investments
+    if (args.length > 0) {
+        const investments = `('` + args.join(`', '`).toUpperCase() + `')`
+        action = `DELETE FROM investment WHERE stockMIC IN ${investments} AND user = ${user}`
+        reply = 'Now those investments are gone.'
+    }
+
+    db.exec(action, async (_) => {
+        await sendMessage(user, reply)
+    })
+}
+
+helps.stock = `
+/stock \\[STOCK ...]
+\`\`\`
 List specified stocks and their last known price.
 Zero arguments lists all stocks.
 I just know stocks that users have invested with /invest.
-
+\`\`\`
 Examples:
-  /linvest
-  /linvest AMD TSLA NVDA`
+\`\`\`
+  /stock
+  /stock AMD TSLA NVDA
+\`\`\``
 
-async function stocks(user, args) {
+async function stock(user, args) {
     var action = `SELECT * FROM stock`
     var reply = 'All stocks that I am aware of```\n'
 
@@ -141,33 +150,34 @@ async function stocks(user, args) {
     })
 }
 
-const HELP = `
-/help [COMMAND ...]
-
+helps.help = + `
+/help \\[COMMAND ...]
+\`\`\`
 Show help for specified commands.
 Zero arguments shows all helps.
-
+\`\`\`
 Examples:
+\`\`\`
   /help
-  /help help stocks`
+  /help help stock
+\`\`\``
 
 async function help(user, args) {
+    // if no arguments then list all commands
     if (args.length == 0) {
         args = Object.keys(commands)
     }
 
-    const helps = { INVEST, LINVESTS, DINVESTS, STOCKS, HELP }
-
     for (const arg of args) {
-        const help = helps[arg.toUpperCase()]
+        const help = helps[arg.toLowerCase()]
         if (help) {
-            await sendMessage(user, '```' + help + '```')
+            await sendMessage(user, help)
         }
     }
 }
 
 const commands = {
-    invest, linvests, dinvests, stocks, help
+    invest, linvest, dinvest, stock, help
 }
 
 // exports
