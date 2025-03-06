b9d46759bee18c61eb31d48dd9b4ead3305f5a2e
pgvalle <pgvalle.nogueira@gmail.com>
2025-03-06 14:42:03 -0300

    refactor

--- src/bot.js
+++ src/bot.js
@@ -1,73 +1,51 @@
 const TelegramBot = require('node-telegram-bot-api') // https://github.com/yagop/node-telegram-bot-api
 
-// get token from environment variable
 const token = process.env.TELEGRAM_BOT_TOKEN
 const bot = new TelegramBot(token, {
     polling: true
 })
 
-// only one user
-const mommy = {
-    id: undefined,
-    timeLastCmd: 0
-}
-
-// the user is going to be the one that sent the first message
-function imprint(msg) {
-    const id = msg.chat.id
-
-    if (!mommy.id) {
-        mommy.id = id
-        sendMsg('Mommyyyyyyyyyyyyyyyy.')
-    } else if (mommy.id != id) {
-        sendMsg('Mommy, a stranger tried to talk to me.')
-    }
-}
+var user = undefined
 
 // send message with markdown formatting
-async function sendMsg(str) {
-    await bot.sendMessage(mommy.id, str, {
+function sendMsg(str) {
+    bot.sendMessage(user, str, {
         parse_mode: 'Markdown'
     })
 }
 
-function setupResponses(cmds) {
-    const msgRegex = /^(?!\/\S).+/s
-
-    // respond to normal messages with this callback
-    bot.onText(msgRegex, (msg, _) => {
-        imprint(msg)
-        sendMsg(msg.text)
-    })
-
-    const cmdRegex = /^\/(?<name>\S+)(?:\s+(?<args>.+))?$/
-    const cmdSpamDelta = 2
-
-    // Respond commands, like /stock, with this callback
-    bot.onText(cmdRegex, (msg, match) => {
-        imprint(msg)
-
-        const timenow = 0.001 * Date.now()
-        const time2wait = cmdSpamDelta - (timenow - user.timeLastCmd)
-        user.timeLastCmd = timenow
-
-        if (time2wait > 0) {
-            sendMsg(`Wait ${time2wait.toFixed(1)}s to send another command.`)
-            return
-        }
-
-        let { name, args } = match.groups
+function setCmds(cmds) {
+    // Respond to given commands
+    bot.onText(/^\/(\S+)(?:\s+(.+))?$/, (msg, match) => {
+        const name = match[1]
+        const args = match[2]?.split(' ') || ' '
         const cmd = cmds[name]
 
         if (cmd) {
-            args = args?.split(' ') || []
             cmd(args)
         } else {
-            sendMsg(`What is ${name}? See /help.`)
+            sendMsg('I dont know that command')
         }
     })
 }
 
+// Only answer to user
+bot.on('message', (msg, _) => {
+    const newUser = msg.chat.id
+
+    if (!user) {
+        user = newUser
+        sendMsg('Mommyyyyyyyyyyyyyyy.')
+    } else if (user != newUser) {
+        sendMsg('Mommy! A stranger tried to talk to me.')
+    }
+})
+
+// if the message is not a command, send it back
+bot.onText(/^(?!\/\S).+/s, (msg, _) => {
+    sendMsg(msg.text)
+})
+
 module.exports = {
-    sendMsg, setupResponses
+    sendMsg, setCmds
 }
