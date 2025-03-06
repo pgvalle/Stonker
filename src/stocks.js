22420611842031b98d9c278bd1dc6bf125a20985
pgvalle <pgvalle.nogueira@gmail.com>
2025-03-06 15:08:36 -0300

    refactoring

--- src/stocks.js
+++ src/stocks.js
@@ -2,40 +2,36 @@ const { db, queries } = require('./db')
 const bot = require('./bot')
 const sock = require('stocksocket') // https://github.com/gregtuc/StockSocket
 
-// Updates stock table each time a new price is know
-function stockUpdater(stock) {
-    const queryParams = {
-        $MIC: stock.id,
-        $price: stock.price,
-        $time:  stock.time,
-        $marketHours: stock.marketHours
-    }
-
-    db.serialize(() => {
-        // insert (first time) or update stock in stock table
-        // use all fields of queryParams
-        db.run(queries.ADD_OR_UPDATE_STOCK, queryParams)
-
-        // notify user if min gain or max loss were reached (out of range)
-        // only use $MIC
-        db.each(queries.GET_NOTIFY_STOCK_INVESTMENTS, queryParams, (_, row) => {
-            bot.sendMsg(row.MIC + ' $' + row.value)
+// Listen to a given stock
+function listenStock(MIC) {
+    // each time MIC updates, execute the callback
+    sock.addTicker(MIC.toUpperCase(), (stock) => {
+        const params = {
+            $MIC: stock.id,
+            $price: stock.price,
+            $time:  stock.time,
+            $marketHours: stock.marketHours
+        }
+    
+        db.serialize(() => {
+            // insert (first time) or update stock in stock table
+            db.run(queries.ADD_OR_UPDATE_STOCK, params)
+            
+            // notify user if investment value got out of range
+            db.each(queries.GET_NOTIFY_STOCK_INVESTMENTS, params, (_, row) => {
+                bot.sendMsg(row.MIC + ' $' + row.value)
+            })
         })
     })
 }
 
-// Listen to a given stock
-function addStock(MIC) {
-    sock.addTicker(MIC.toUpperCase(), stockUpdater)
-}
-
-// Users may try to invest on invalid stocks. That ends up adding invalid listeners.
+// User may try to invest on invalid stocks. That ends up adding invalid listeners.
 function refreshStockListeners() {
-    sock.removeAllTickers() // Trash all listeners
+    sock.removeAllTickers()
 
     // If a listener had an entry in stock table, it was valid. Readd it.
     db.each(queries.GET_ALL_STOCKS, (_, row) => {
-        addStock(row.MIC)
+        listenStock(row.MIC)
     })
 
     // Do this forever
@@ -52,5 +48,5 @@ function fmtStock(s) {
 }
 
 module.exports = {
-    addStock, refreshStockListeners, fmtInvestment, fmtStock
+    listenStock, refreshStockListeners, fmtInvestment, fmtStock
 }
