8f94713bc7626953236876de2407fdd2cfcc42be
pgvalle <pgvalle.nogueira@gmail.com>
2025-03-06 10:00:47 -0300

    refactoring

--- src/stocks.js
+++ src/stocks.js
@@ -5,19 +5,21 @@ const sock = require('stocksocket') // https://github.com/gregtuc/StockSocket
 // Updates stock table each time a new price is know
 function stockUpdater(stock) {
     db.serialize(() => {
-        // insert (first time) or update stock in stock table
-        db.run(queries.ADD_OR_UPDATE_STOCK, {
+        const params = {
             $MIC: stock.id,
             $price: stock.price,
             $time:  stock.time,
             $marketHours: stock.marketHours
-        })
+        }
+
+        // insert (first time) or update stock in stock table
+        // use all fields of params
+        db.run(queries.ADD_OR_UPDATE_STOCK, params)
 
         // notify user if min gain or max loss were reached (out of range)
-        db.each(queries.GET_NOTIFY_STOCK_INVESTMENTS, {
-            $MIC: stock.id
-        }, (err, row) => {
-            bot.sendMsg(row.user, row.MIC + ' $' + row.value)
+        // only use $MIC
+        db.each(queries.GET_NOTIFY_STOCK_INVESTMENTS, params, (_, row) => {
+            bot.sendMsg(row.MIC + ' $' + row.value)
         })
     })
 }
@@ -32,7 +34,7 @@ function refreshStockListeners() {
     sock.removeAllTickers() // Trash all listeners
 
     // If a listener had an entry in stock table, it was valid. Readd it.
-    db.each(queries.GET_ALL_STOCKS, (err, row) => {
+    db.each(queries.GET_ALL_STOCKS, (_, row) => {
         addStock(row.MIC)
     })
 
