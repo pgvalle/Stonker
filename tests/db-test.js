410a1b316293c88f1192a5a3edc65cbada65d8c6
pgvalle <pgvalle.nogueira@gmail.com>
2025-04-08 11:45:19 -0300

    tests

--- tests/db-test.js
+++ tests/db-test.js
@@ -1,10 +1,6 @@
 const { db, queries } = require('../src/db')
-const stocks = require('../src/stocks')
+// const stocks = require('../src/stocks')
 
 db.serialize(() => {
-    db.run(queries.CREATE_TABLES)
-
-    db.each(queries.GET_STOCKS, (err, row) => {
-        console.log(row)
-    })
+    db.run(queries.ADD_STOCK, { $MIC: "Hello" })
 })
