5ec550d509241eae3e4630a9af51ccf2abb63119
pgvalle <pgvalle.nogueira@gmail.com>
2025-04-08 20:45:10 -0300

    refactoring and implemented invest query

--- src/db.js
+++ src/db.js
@@ -1,12 +1,11 @@
-const sql = require('sqlite3')
+const sql = require('sqlite3');
 
-const db = new sql.Database('./stocks.db', sql.OPEN_CREATE | sql.OPEN_READWRITE)
+const db = new sql.Database('./stocks.db', sql.OPEN_CREATE | sql.OPEN_READWRITE);
 
 // utility to make queries easier
-db.query = async function (sql, params) {
-    const that = this;
+function dbQuery(sql, params) {
     return new Promise((resolve, reject) => {
-        that.all(sql, params, (error, rows) => {
+        db.all(sql, params, (error, rows) => {
             if (error) reject(error);
             else resolve(rows);
         });
@@ -14,7 +13,7 @@ db.query = async function (sql, params) {
 }
 
 exports.init = async function () {
-    await db.query(`
+    await dbQuery(`
     CREATE TABLE IF NOT EXISTS investment (
         stockMIC     VARCHAR(8) NOT NULL,
         stockPrice   REAL,
@@ -32,59 +31,70 @@ exports.init = async function () {
     FOR EACH ROW BEGIN
         UPDATE investment SET value = OLD.value * NEW.stockPrice / OLD.stockPrice
         WHERE OLD.value IS NOT NULL AND OLD.stockPrice IS NOT NULL;
-    END;`)
+    END;`);
 }
 
 exports.addStock = async function (mic) {
-    const sql = 'INSERT INTO investment (stockMIC) VALUES (upper($MIC))'
-    return await db.query(sql, { $MIC: mic })
+    const sql = 'INSERT INTO investment (stockMIC) VALUES ($mic)';
+    return await dbQuery(sql, { $mic: mic });
 }
 
 exports.delStock = async function (mic) {
-    const sql = 'DELETE FROM investment WHERE stockMIC = upper($MIC) RETURNING *'
-    return await db.query(sql, { $MIC: mic })
+    const sql = 'DELETE FROM investment WHERE stockMIC = $mic RETURNING *';
+    return await dbQuery(sql, { $mic: mic });
 }
 
 exports.getStocks = async function () {
-    return await db.query('SELECT * FROM investment')
+    return await dbQuery('SELECT * FROM investment ORDER BY stockPrice DESC');
 }
 
 exports.updateStock = async function (data) {
-    const sql1 = 'UPDATE investment SET stockPrice = $price WHERE stockMIC = $MIC'
-    await db.query(sql1, { $MIC: data.id, $price: data.price })
+    const sql1 = 'UPDATE investment SET stockPrice = $price WHERE stockMIC = $mic';
+    await dbQuery(sql1, { $mic: data.id, $price: data.price });
 
+    const cond = '((value - firstValue) BETWEEN minValue AND maxValue)';
     const sql2 = `
-    UPDATE investment SET valueInRange = (value - firstValue) BETWEEN minValue AND maxValue
-    WHERE ((value - firstValue) BETWEEN minValue AND maxValue) != valueInRange AND stockMIC = $MIC
-    RETURNING *`
-    return await db.query(sql2, { $MIC: data.id })
+    UPDATE investment SET valueInRange = ${cond}
+    WHERE ${cond} != valueInRange AND stockMIC = $mic
+    RETURNING *`;
+
+    return await dbQuery(sql2, { $mic: data.id });
+}
+
+exports.invest = async function (mic, value, diff) {
+    const sql = `
+    UPDATE investment SET
+        firstValue = $value,
+        value = $value,
+        minValue = $minValue,
+        maxValue = $maxValue,
+        valueInRange = TRUE
+    WHERE stockMIC = $mic
+    RETURNING *`;
+
+    return await dbQuery(sql, {
+        $mic: mic,
+        $value: value,
+        $minValue: value - diff,
+        $maxValue: value + diff
+    });
 }
 
 /*
 queries.ADD_OR_UPDATE_STOCK = `
     INSERT INTO stock (MIC, price, time, marketHours)
-    VALUES (upper($MIC), $price, $time, $marketHours)
+    VALUES ($mic, $price, $time, $marketHours)
     ON CONFLICT (MIC) DO UPDATE SET
         price = $price,
         time = $time,
         marketHours = $marketHours`
 */
-const queries = {}
-queries.CAN_INVEST = `SELECT * FROM investment WHERE stockMIC = $MIC AND stockPrice IS NOT NULL`
-queries.INVEST = `
-UPDATE investment SET
-    firstValue = $value,
-    value = $value,
-    minValue = $minValue,
-    maxValue = $maxValue,
-    valueInRange = TRUE`
 
 // queries.LIST = `
 // SELECT * FROM investment WHERE stockMIC IN ${MICs}
 // ORDER BY value - firstValue DESC LIMIT $limit`
 
 /*
-const MICs = `(SELECT upper(value) FROM json_each($MICs))`
 
 queries.GET_ALL_STOCKS = `SELECT * FROM stock`
 queries.GET_STOCKS = `SELECT * FROM stock ORDER BY price DESC LIMIT $limit`
@@ -94,7 +104,7 @@ queries.GET_SPECIFIC_STOCKS = `SELECT * FROM stock WHERE MIC IN ${MICs} ORDER BY
 // should notify when min gain or max loss were reached (out of range)
 queries.GET_NOTIFY_STOCK_INVESTMENTS = `
     UPDATE investment SET valueInRange = (value - firstValue ) BETWEEN minValue AND maxValue
-    WHERE ((value - firstValue) BETWEEN minValue AND maxValue) != valueInRange AND MIC = $MIC
+    WHERE ((value - firstValue) BETWEEN minValue AND maxValue) != valueInRange AND MIC = $mic
     RETURNING *`
 
 // order by greatest absolute gain
@@ -111,7 +121,7 @@ queries.DEL_SPECIFIC_USER_INVESTMENTS = `DELETE FROM investment WHERE user = $us
 
 queries.ADD_OR_UPDATE_STOCK = `
     INSERT INTO stock (MIC, price, time, marketHours)
-    VALUES (upper($MIC), $price, $time, $marketHours)
+    VALUES ($mic, $price, $time, $marketHours)
     ON CONFLICT (MIC) DO UPDATE SET
         price = $price,
         time = $time,
@@ -119,5 +129,5 @@ queries.ADD_OR_UPDATE_STOCK = `
 
 queries.ADD_OR_UPDATE_INVESTMENT = `
     INSERT OR REPLACE INTO investment (MIC, user, firstValue , value, minValue, maxValue, valueInRange)
-    VALUES (upper($MIC), $user, $value, $value, $minValue, $maxValue, TRUE)`
+    VALUES ($mic, $user, $value, $value, $minValue, $maxValue, TRUE)`
 */
