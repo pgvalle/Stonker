99eeadc536d970590bfeeca559c63bbfcc154d93
pgvalle <pgvalle.nogueira@gmail.com>
2025-04-08 14:02:17 -0300

    testing new queries

--- tests/db-test.js
+++ tests/db-test.js
@@ -1,6 +1,14 @@
-const { db, queries } = require('../src/db')
+const db = require('../src/db')
 // const stocks = require('../src/stocks')
 
-db.serialize(() => {
-    db.run(queries.ADD_STOCK, { $MIC: "Hello" })
-})
+async function main() {
+    await db.init()
+    await db.addStock('hello')
+    var a = await db.getStocks()
+    console.log(a)
+    await db.delStock('hello')
+    a = await db.getStocks()
+    console.log(a)
+}
+
+main()
