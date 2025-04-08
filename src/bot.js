41b40fa282a480b730a356352165fb4a57df41fb
pgvalle <pgvalle.nogueira@gmail.com>
2025-04-08 11:45:14 -0300

    a

--- src/bot.js
+++ src/bot.js
@@ -1,24 +1,70 @@
 const TelegramBot = require('node-telegram-bot-api') // https://github.com/yagop/node-telegram-bot-api
 
-// get token from environment variable
 const TOKEN = process.env.TELEGRAM_BOT_TOKEN
 const bot = new TelegramBot(TOKEN, {
     polling: true
 })
 
-// respond to plain messages. Just repeat what the user says.
-bot.onText(/^(?!\/\S).+/s, (msg, _) => {
-    const user = msg.chat.id
-    sendMsg(user, msg.text)
-})
+var mommy = null
 
-// send message with markdown formatting
-async function sendMsg(user, str) {
-    await bot.sendMessage(user, str, {
+function ensureMommy() {
+    console.assert(mommy, "I don't have a mommy")
+}
+
+// send message to mommy with markdown formatting
+async function sendMsg(str) {
+    ensureMommy()
+    await bot.sendMessage(mommy, str, {
         parse_mode: 'Markdown'
     })
 }
 
-module.exports = {
-    sendMsg
+// commands
+
+const { db, queries } = require('./db')
+const cmds = {}
+
+cmds.a = (args) => {
+    ensureMommy()
+    if (args.length != 1) {
+        bot.sendMsg('Give 1 argument')
+        return
+    }
+
+    // ...
 }
+
+// /a
+// /d
+// /l
+// /i
+
+// responding user
+
+const MSG_REGEX = /^(?!\/\S).+/s
+const CMD_REGEX = /^\/(?<name>\S+)(?:\s+(?<args>.+))?$/
+
+// respond to plain messages. Just repeat what the user says.
+bot.onText(MSG_REGEX, (msg) => {
+    const user = msg.chat.id
+    if (!mommy) mommy = user // respond to only one user. Imprint
+    sendMsg(user, msg.text)
+})
+
+// respond to commands defined
+bot.onText(CMD_REGEX, (msg, match) => {
+    ensureMommy()
+
+    const user = msg.chat.id
+    if (user != mommy) return
+
+    const cmdName = match.groups.name
+    const cmdArgs = match.groups.args?.split(' ') || []
+    const cmd = cmds[cmdName]
+
+    if (cmd) {
+        cmd(user, cmdArgs)
+    } else {
+        sendMsg(`What is ${cmdName}? See /help.`)
+    }
+})
