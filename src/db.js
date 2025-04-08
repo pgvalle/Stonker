65e7be6e820260838fd7f02ae3c74c5e04dc5ca7
pgvalle <pgvalle.nogueira@gmail.com>
2025-04-08 14:02:08 -0300

    finally good queries

--- src/db.js
+++ src/db.js
@@ -1,38 +1,64 @@
 const sql = require('sqlite3')
 
 const db = new sql.Database('./stocks.db', sql.OPEN_CREATE | sql.OPEN_READWRITE)
-const queries = {}
 
-const MICs = `(SELECT upper(value) FROM json_each($MICs))`
+// utility to make queries easier
+db.query = async function (sql, params) {
+    const that = this;
+    return new Promise((resolve, reject) => {
+        that.all(sql, params, (error, rows) => {
+            if (error) reject(error);
+            else resolve(rows);
+        });
+    });
+}
 
-// create database
-db.run(`
-CREATE TABLE IF NOT EXISTS investment (
-    stockMIC     VARCHAR(8) NOT NULL,
-    stockPrice   REAL,
-    firstValue   REAL,
-    value        REAL,
-    minValue     REAL,
-    maxValue     REAL,
-    valueInRange INTEGER,
-    PRIMARY KEY (stockMIC)
-);
+exports.init = async function () {
+    await db.query(`
+    CREATE TABLE IF NOT EXISTS investment (
+        stockMIC     VARCHAR(8) NOT NULL,
+        stockPrice   REAL,
 
-CREATE TRIGGER IF NOT EXISTS updateInvestmentValue
-AFTER UPDATE ON investment.stockPrice
-FOR EACH ROW BEGIN
-    UPDATE investment SET value = OLD.value * NEW.stockPrice / OLD.stockPrice
-    WHERE OLD.value IS NOT NULL AND OLD.stockPrice IS NOT NULL;
-END;`)
+        firstValue   REAL,
+        value        REAL,
+        minValue     REAL,
+        maxValue     REAL,
+        valueInRange INTEGER,
+        PRIMARY KEY (stockMIC)
+    );
 
-queries.ADD_STOCK = `INSERT INTO investment (stockMIC) VALUES (upper($MIC))`
-queries.DEL_STOCK = `DELETE FROM investment WHERE stockMIC = upper($MIC) RETURNING *`
-queries.UPDATE_STOCK = `UPDATE investment SET stockPrice = $price`
+    CREATE TRIGGER IF NOT EXISTS updateInvestmentValue
+    AFTER UPDATE ON investment.stockPrice
+    FOR EACH ROW BEGIN
+        UPDATE investment SET value = OLD.value * NEW.stockPrice / OLD.stockPrice
+        WHERE OLD.value IS NOT NULL AND OLD.stockPrice IS NOT NULL;
+    END;`)
+}
 
-queries.GET_WHICH_NOTIFY = `
-UPDATE investment SET valueInRange = (value - firstValue) BETWEEN minValue AND maxValue
-WHERE ((value - firstValue) BETWEEN minValue AND maxValue) != valueInRange AND stockMIC = $MIC
-RETURNING *`
+exports.addStock = async function (mic) {
+    const sql = 'INSERT INTO investment (stockMIC) VALUES (upper($MIC))'
+    return await db.query(sql, { $MIC: mic })
+}
+
+exports.delStock = async function (mic) {
+    const sql = 'DELETE FROM investment WHERE stockMIC = upper($MIC) RETURNING *'
+    return await db.query(sql, { $MIC: mic })
+}
+
+exports.getStocks = async function () {
+    return await db.query('SELECT * FROM investment')
+}
+
+exports.updateStock = async function (data) {
+    const sql1 = 'UPDATE investment SET stockPrice = $price WHERE stockMIC = $MIC'
+    await db.query(sql1, { $MIC: data.id, $price: data.price })
+
+    const sql2 = `
+    UPDATE investment SET valueInRange = (value - firstValue) BETWEEN minValue AND maxValue
+    WHERE ((value - firstValue) BETWEEN minValue AND maxValue) != valueInRange AND stockMIC = $MIC
+    RETURNING *`
+    return await db.query(sql2, { $MIC: data.id })
+}
 
 /*
 queries.ADD_OR_UPDATE_STOCK = `
@@ -43,7 +69,7 @@ queries.ADD_OR_UPDATE_STOCK = `
         time = $time,
         marketHours = $marketHours`
 */
-
+const queries = {}
 queries.CAN_INVEST = `SELECT * FROM investment WHERE stockMIC = $MIC AND stockPrice IS NOT NULL`
 queries.INVEST = `
 UPDATE investment SET
@@ -53,11 +79,9 @@ UPDATE investment SET
     maxValue = $maxValue,
     valueInRange = TRUE`
 
-queries.LIST = `
-SELECT * FROM investment WHERE stockMIC IN ${MICs}
-ORDER BY value - firstValue DESC LIMIT $limit`
-
-module.exports = { db, queries }
+// queries.LIST = `
+// SELECT * FROM investment WHERE stockMIC IN ${MICs}
+// ORDER BY value - firstValue DESC LIMIT $limit`
 
 /*
 const MICs = `(SELECT upper(value) FROM json_each($MICs))`
