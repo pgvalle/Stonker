643b6d41a1af4d3ff4e3df46e87c3fa58f52b348
pgvalle <pgvalle.nogueira@gmail.com>
2025-04-08 21:40:39 -0300

    a

--- src/bot.js
+++ src/bot.js
@@ -1,86 +1,72 @@
-const TelegramBot = require('node-telegram-bot-api') // https://github.com/yagop/node-telegram-bot-api
-const sock = require('stocksocket') // https://github.com/gregtuc/StockSocket
+const TelegramBot = require('node-telegram-bot-api'); // https://github.com/yagop/node-telegram-bot-api
+const sock = require('stocksocket'); // https://github.com/gregtuc/StockSocket
+const db = require('./db');
 
-const TOKEN = process.env.TELEGRAM_BOT_TOKEN
-const bot = new TelegramBot(TOKEN, {
-    polling: true
-})
+const TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const bot = new TelegramBot(TOKEN, { polling: true });
 
-var user = null
+var user = null;
 
 // send message to user with markdown formatting
 async function sendMsg(str) {
-    await bot.sendMessage(user, str, {
-        parse_mode: 'Markdown'
-    })
+    await bot.sendMessage(user, str, { parse_mode: 'Markdown' });
 }
-
-// stock monitoring
-
-
 
 // commands
 
-const { db, queries } = require('./db')
-const cmds = {}
+const cmds = {};
 
 cmds.a = (args) => {
-    if (args.length != 1) {
-        bot.sendMsg('Give 1 argument')
-        return
-    }
-
-    db.run(queries.ADD_STOCK, { $MIC: args[0] }, (res, err) => {
-        if (err) sendMsg("Error")
-        else sendMsg("Alright")
-    })
+    sendMsg("Not implemented yet");
 }
 
 cmds.d = (args) => {
-    if (args.length != 1) {
-        bot.sendMsg('Give 1 argument')
-        return
-    }
-
-    db.run(queries.DEL_STOCK, { $MIC: args[0] }, (res, err) => {
-        if (err) sendMsg("Error")
-        else sendMsg("Alright")
-    })
+    sendMsg("Not implemented yet");
 }
 
 cmds.l = (args) => {
-    sendMsg("Not implemented yet")
+    sendMsg("Not implemented yet");
 }
 
 cmds.i = (args) => {
-    sendMsg("Not implemented yet")
+    sendMsg("Not implemented yet");
 }
 
 // responding user
 
-const MSG_REGEX = /^(?!\/\S).+/s
-const CMD_REGEX = /^\/(?<name>\S+)(?:\s+(?<args>.+))?$/
+const MSG_REGEX = /^(?!\/\S).+/s;
+const CMD_REGEX = /^\/(?<name>\S+)(?:\s+(?<args>.+))?$/;
 
 // respond to plain messages. Just repeat what the user says.
 bot.onText(MSG_REGEX, (msg) => {
-    if (!user) user = msg.chat.id
+    if (!user) user = msg.chat.id;
     if (user != msg.chat.id) return;
-    
-    sendMsg(user, msg.text)
-})
+    console.log('hello')
+    sendMsg(msg.text);
+});
 
 // respond to commands defined
 bot.onText(CMD_REGEX, (msg, match) => {
-    if (!user) user = msg.chat.id
+    if (!user) user = msg.chat.id;
     if (user != msg.chat.id) return;
 
-    const cmdName = match.groups.name
-    const cmdArgs = match.groups.args?.split(' ') || []
-    const cmd = cmds[cmdName]
+    const cmdName = match.groups.name;
+    const cmdArgs = match.groups.args?.split(' ') || [];
+    const cmd = cmds[cmdName];
 
     if (cmd) {
-        cmd(cmdArgs)
+        cmd(cmdArgs);
     } else {
-        sendMsg(`What is ${cmdName}?`)
+        sendMsg(`What is ${cmdName}?`);
     }
-})
+});
+
+(async () => {
+    await db.init()
+
+    // get all stocks stored in db and add their tickers
+    const stocks = await db.getStocks();
+    for (const stock in stocks) {
+        sock.addTicker(stock.stockMIC, db.updateStock);
+    }
+})();
