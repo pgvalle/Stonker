ef879c4a069a06ac5eba94a771c44730507af8e5
pgvalle <pgvalle.nogueira@gmail.com>
2025-04-09 13:28:02 -0300

    in case of error (adding stock that already exists in database

--- src/db.js
+++ src/db.js
@@ -30,8 +30,13 @@ db.prepare(`
 // EXPORTS
 
 exports.addStock = function (mic) {
-    const query = 'INSERT INTO investment (stockMIC) VALUES (@mic) RETURNING *'
-    return db.prepare(query).get({ mic })
+    const query = 'INSERT INTO investment (stockMIC) VALUES (@mic)'
+    try {
+        db.prepare(query).run({ mic })
+        return true
+    } catch (error) {
+        return false
+    }
 }
 
 exports.delStock = function (mic) {
@@ -67,57 +72,5 @@ exports.invest = function (mic, value, diff) {
     WHERE stockMIC = @mic AND stockPrice IS NOT NULL
     RETURNING *`
 
-    return db.prepare(query).get({ mic, value, diff })
+    db.prepare(query).get({ mic, value, diff })
 }
-
-/*
-queries.ADD_OR_UPDATE_STOCK = `
-    INSERT INTO stock (MIC, price, time, marketHours)
-    VALUES ($mic, $price, $time, $marketHours)
-    ON CONFLICT (MIC) DO UPDATE SET
-        price = $price,
-        time = $time,
-        marketHours = $marketHours`
-*/
-
-// queries.LIST = `
-// SELECT * FROM investment WHERE stockMIC IN ${MICs}
-// ORDER BY value - firstValue DESC LIMIT $limit`
-
-/*
-
-queries.GET_ALL_STOCKS = `SELECT * FROM stock`
-queries.GET_STOCKS = `SELECT * FROM stock ORDER BY price DESC LIMIT $limit`
-queries.GET_SPECIFIC_STOCKS = `SELECT * FROM stock WHERE MIC IN ${MICs} ORDER BY price DESC LIMIT $limit`
-
-// get investments from which a notification must be generated
-// should notify when min gain or max loss were reached (out of range)
-queries.GET_NOTIFY_STOCK_INVESTMENTS = `
-    UPDATE investment SET valueInRange = (value - firstValue ) BETWEEN minValue AND maxValue
-    WHERE ((value - firstValue) BETWEEN minValue AND maxValue) != valueInRange AND MIC = $mic
-    RETURNING *`
-
-// order by greatest absolute gain
-queries.GET_USER_INVESTMENTS = `
-    SELECT * FROM investment WHERE user = $user
-    ORDER BY value - firstValue DESC LIMIT $limit`
-
-queries.GET_SPECIFIC_USER_INVESTMENTS = `
-    SELECT * FROM investment WHERE MIC IN ${MICs} AND user = $user
-    ORDER BY value - firstValue DESC LIMIT $limit`
-
-queries.DEL_ALL_USER_INVESTMENTS = `DELETE FROM investment WHERE user = $user RETURNING *`
-queries.DEL_SPECIFIC_USER_INVESTMENTS = `DELETE FROM investment WHERE user = $user AND MIC IN ${MICs} RETURNING *`
-
-queries.ADD_OR_UPDATE_STOCK = `
-    INSERT INTO stock (MIC, price, time, marketHours)
-    VALUES ($mic, $price, $time, $marketHours)
-    ON CONFLICT (MIC) DO UPDATE SET
-        price = $price,
-        time = $time,
-        marketHours = $marketHours`
-
-queries.ADD_OR_UPDATE_INVESTMENT = `
-    INSERT OR REPLACE INTO investment (MIC, user, firstValue , value, minValue, maxValue, valueInRange)
-    VALUES ($mic, $user, $value, $value, $minValue, $maxValue, TRUE)`
-*/
